4856effb5df0f4ba9ac71b62170fb247e9ffdd36
am08online <83348151+am08online@users.noreply.github.com>
2021-05-28 20:54:35 +0200

    modifsAmazin
    Projet Amazin - Qu.estionnaire Satisfaction Client

---
+++ step4.php
@@ -0,0 +1,11 @@
+<!-- Etape 4 : A afficher uniquement si "non" a été répondu à l'étape 3 -->
+<p>Votre problème n'a pas été résolu.</p>
+    <p>Pour être rappelé, entrez votre numéro de téléphone dans le clavier virtuel et validez :</p>
+    <!-- Coder ici un clavier numérique permettant de saisir le numéro de téléphone -->
+
+<!-- Afficher ici le numéro de téléphone qui s'affiche au fur et à mesure de la saisie-->
+    <p>Votre numéro : #numéro de téléphone saisi#</p>
+    <a href="" role="button" class="btn btn-success">Valider</a> <!-- Validation du numéro de téléphone -->
+
+<!-- Affichage du bouton "Ne pas être rappelé" -->
+    <a href="?step=5&result=<?= $_GET['result'] ?>" role="button" class="btn btn-outline-danger">Ne pas être rappelé</a>
